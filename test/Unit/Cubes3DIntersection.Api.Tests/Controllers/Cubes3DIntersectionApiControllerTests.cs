395d7f03cd2e3e73528cb17189b7b6b0619a0b89
daniele.crivello <daniele.crivello84@libero.it>
2020-06-01 22:04:29 +0200

    Added Unit Tests corresponding to Postman Tests already present

--- test/Unit/Cubes3DIntersection.Api.Tests/Controllers/Cubes3DIntersectionApiControllerTests.cs
+++ test/Unit/Cubes3DIntersection.Api.Tests/Controllers/Cubes3DIntersectionApiControllerTests.cs
@@ -19,25 +19,25 @@ namespace Cubes3DIntersection.Api.Tests.Controllers
 
         public Cubes3DIntersectionApiControllerTests(CustomWebApplicationFactory<Startup> factory)
         {
-            _mockCube3DIntersectionService =  new Mock<ICube3DIntersectionService>();
+            _mockCube3DIntersectionService = new Mock<ICube3DIntersectionService>();
             _mockAppLogger = new Mock<IAppLogger<Cubes3DIntersectionApiController>>();
         }
 
         [Fact]
-        public async Task PostCubes3DIntersection_StateUnderTest_ExpectedBehavior()
+        public async Task PostCubes3DIntersectionShouldTouch()
         {
             // Arrange
             var cubes3DIntersectionApiController = CreateCubes3DIntersectionApiController();
 
-            var  firstCube3D = new Cube3DModel
-                {
-                    PointCoordinates = PointModelFactory.Create(2, 2, 2)
-                };
+            var firstCube3D = new Cube3DModel
+            {
+                PointCoordinates = PointModelFactory.Create(2, 2, 2)
+            };
 
             var secondCube3D = new Cube3DModel
-                {
-                    PointCoordinates = PointModelFactory.Create(4, 2, 2)
-                };
+            {
+                PointCoordinates = PointModelFactory.Create(4, 2, 2)
+            };
 
             var edgesLength = 2;
 
@@ -54,6 +54,118 @@ namespace Cubes3DIntersection.Api.Tests.Controllers
                 SecondCube3D = secondCube3D,
                 EdgesLength = edgesLength,
                 Collision = true,
+                IntersectionVolume = 0
+            };
+
+            _mockCube3DIntersectionService
+                .Setup(ci => ci.Create(cube3DIntersectionRequest))
+                .ReturnsAsync(cube3DIntersectionExpectedResponse);
+
+            // Act
+            var createdResponse = await cubes3DIntersectionApiController.PostCubes3DIntersection(
+                cube3DIntersectionRequest);
+
+            Assert.NotNull(createdResponse);
+
+            var result = createdResponse.Result as ObjectResult;
+            Assert.Equal(result.StatusCode, (int)HttpStatusCode.Created);
+
+            var actualResult = result.Value as Cube3DIntersectionModel;
+            Assert.IsType<Cube3DIntersectionModel>(actualResult);
+
+            Assert.True(actualResult.Collision == cube3DIntersectionExpectedResponse.Collision);
+            Assert.True(actualResult.IntersectionVolume == cube3DIntersectionExpectedResponse.IntersectionVolume);
+
+            _mockCube3DIntersectionService.VerifyAll();
+        }
+
+        [Fact]
+        public async Task PostCubes3DIntersectionShouldOverlap()
+        {
+            // Arrange
+            var cubes3DIntersectionApiController = CreateCubes3DIntersectionApiController();
+
+            var firstCube3D = new Cube3DModel
+            {
+                PointCoordinates = PointModelFactory.Create(2, 2, 2)
+            };
+
+            var secondCube3D = new Cube3DModel
+            {
+                PointCoordinates = PointModelFactory.Create(4, 2, 2)
+            };
+
+            var edgesLength = 2;
+
+            var cube3DIntersectionRequest = new Cube3DIntersectionModel
+            {
+                FirstCube3D = firstCube3D,
+                SecondCube3D = secondCube3D,
+                EdgesLength = edgesLength
+            };
+
+            var cube3DIntersectionExpectedResponse = new Cube3DIntersectionModel
+            {
+                FirstCube3D = firstCube3D,
+                SecondCube3D = secondCube3D,
+                EdgesLength = edgesLength,
+                Collision = true,
+                IntersectionVolume = 4
+            };
+
+            _mockCube3DIntersectionService
+                .Setup(ci => ci.Create(cube3DIntersectionRequest))
+                .ReturnsAsync(cube3DIntersectionExpectedResponse);
+
+            // Act
+            var createdResponse = await cubes3DIntersectionApiController.PostCubes3DIntersection(
+                cube3DIntersectionRequest);
+
+            Assert.NotNull(createdResponse);
+
+            var result = createdResponse.Result as ObjectResult;
+            Assert.Equal(result.StatusCode, (int)HttpStatusCode.Created);
+
+            var actualResult = result.Value as Cube3DIntersectionModel;
+            Assert.IsType<Cube3DIntersectionModel>(actualResult);
+
+            Assert.True(actualResult.Collision == cube3DIntersectionExpectedResponse.Collision);
+            Assert.True(actualResult.IntersectionVolume == cube3DIntersectionExpectedResponse.IntersectionVolume);
+
+            _mockCube3DIntersectionService.VerifyAll();
+        }
+
+        [Fact]
+        public async Task PostCubes3DIntersectionShouldNotTouch()
+        {
+            // Arrange
+            var cubes3DIntersectionApiController = CreateCubes3DIntersectionApiController();
+
+            var firstCube3D = new Cube3DModel
+            {
+                PointCoordinates = PointModelFactory.Create(2, 2, 2)
+            };
+
+            var secondCube3D = new Cube3DModel
+            {
+                PointCoordinates = PointModelFactory.Create(10, 10, 10)
+            };
+
+            var edgesLength = 2;
+
+            var cube3DIntersectionRequest = new Cube3DIntersectionModel
+            {
+                FirstCube3D = firstCube3D,
+                SecondCube3D = secondCube3D,
+                EdgesLength = edgesLength
+            };
+
+            var cube3DIntersectionExpectedResponse = new Cube3DIntersectionModel
+            {
+                FirstCube3D = firstCube3D,
+                SecondCube3D = secondCube3D,
+                EdgesLength = edgesLength,
+                Collision = false,
                 IntersectionVolume = 0
             };
 
